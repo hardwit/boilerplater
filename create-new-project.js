43fd93b1efd52706dd284dde4a28d808f78e903c
hardwit <minakov@doctornet.pro>
2017-01-22 20:17:44 +0300

    init commit

---
+++ create-new-project.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const zlib = require('zlib');
+const readline = require('readline');
+
+let filesList, projectStructure;
+
+const askForBoilerplateName = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+askForBoilerplateName.question('Boilerplate name: ', name => {
+    if (!name) {
+        console.log('Wrong boilerplate name!');
+        askForBoilerplateName.close();
+
+        return;
+    }
+
+    let templatesDirPath = __dirname + '/templates/' + name;
+
+    if (!fs.existsSync(templatesDirPath)) {
+        console.log('"' + name + '": boilerplate not exist');
+        askForBoilerplateName.close();
+
+        return;
+    }
+
+    if (fs.existsSync(templatesDirPath + '/files.js.gz')) {
+        fs.writeFileSync(templatesDirPath + '/files.js', zlib.gunzipSync(fs.readFileSync(templatesDirPath + '/files.js.gz')));
+    } else {
+        console.log('files.js.gz not found in template directory');
+        askForBoilerplateName.close();
+
+        return;
+    }
+
+    if (fs.existsSync(templatesDirPath + '/projectStructure.js.gz')) {
+        fs.writeFileSync(templatesDirPath + '/projectStructure.js', zlib.gunzipSync(fs.readFileSync(templatesDirPath + '/projectStructure.js.gz')));
+    } else {
+        console.log('projectStructure.js.gz not found in template directory');
+        askForBoilerplateName.close();
+
+        return;
+    }
+
+    filesList = require(templatesDirPath + '/files.js').files;
+    projectStructure = require(templatesDirPath + '/projectStructure.js').projectStructure;
+
+    console.log('"' + name + '" template selected...' );
+
+    createProject(projectStructure.directories, process.cwd());
+
+    projectStructure.files.forEach(function(fileName) {
+        createFile(fileName, filesList[fileName])
+    });
+
+    fs.unlinkSync(templatesDirPath + '/files.js');
+    fs.unlinkSync(templatesDirPath + '/projectStructure.js');
+    askForBoilerplateName.close();
+});
+
+function createProject(directories, path) {
+    directories.forEach(function(dir) {
+        let currentPath = path ? path + '/' + dir.name : process.cwd() + '/' + dir.name;
+
+        fs.mkdir(currentPath, function() {
+            if (dir.files) {
+                dir.files.forEach(function(file) {
+                    let fileName = currentPath.slice(process.cwd().length + 1) + '/' + file;
+
+                    createFile(fileName, filesList[fileName]);
+                });
+            }
+        });
+
+        if (dir.directories) {
+            createProject(dir.directories, currentPath);
+        }
+    });
+}
+
+function createFile(fileName, fileText) {
+    let text = fileText ? fileText : '';
+
+    fs.writeFile(fileName, text, function(error) {
+        console.log(fileName);
+        if (error) {
+            console.log(error);
+            console.log('File ' + fileName + ' not created');
+        }
+    });
+}
---
+++ scan-project.js
@@ -0,0 +1,151 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const zlib = require('zlib');
+const toSource = require('tosource-polyfill');
+const readline = require('readline');
+
+let filesList = {};
+let projectStructure = {};
+let newBoilerplateName = 'Main';
+
+const askForBoilerplateName = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+askForBoilerplateName.question('Boilerplate name: ', name => {
+    if (name) {
+        newBoilerplateName = name;
+    }
+
+    readDirectory(process.cwd(), projectStructure);
+    createTemplates();
+
+    askForBoilerplateName.close();
+});
+
+function readDirectory(path, initialState) {
+    fs.readdirSync(path).forEach(function(elem) {
+        let newState;
+
+        if (elem.includes('.')) {
+            try {
+                updateFilesList(elem, path + '/');
+                updateProjectStructure(elem, initialState, 'file');
+            } catch (err) {
+                readDirectory(path + '/' + elem, initialState);
+            }
+        } else {
+            try {
+               newState = updateProjectStructure(elem, initialState, 'dir');
+
+                readDirectory(path + '/' + elem, newState);
+            } catch (err) {
+                // console.log(err);
+            }
+        }
+    });
+};
+
+function updateFilesList(fileName, path) {
+    filesList[(path + fileName).slice(process.cwd().length + 1)] = fs.readFileSync(path + fileName).toString('utf8');
+};
+
+function updateProjectStructure(elemName, initialState, type) {
+    if (type === 'file') {
+        if (!initialState.files) {
+            initialState.files = [];
+            initialState.files.push(elemName);
+
+            return;
+        }
+
+        initialState.files.push(elemName);
+    }
+
+    if (type === 'dir') {
+        if (!initialState.directories) {
+            initialState.directories = [];
+        }
+
+        initialState.directories.push({
+            name: elemName,
+            files: []
+        });
+
+        let newState;
+
+        initialState.directories.forEach(function(dir) {
+            if (dir.name === elemName) {
+                newState = dir;
+            }
+        });
+
+        return newState;
+    }
+};
+
+function createTemplates() {
+    let filesTemplate = 'exports.files = ';
+    let projectStructureTemplate = 'exports.projectStructure = ';
+    let templatesDirPath = __dirname + '/templates/' + newBoilerplateName;
+
+    if (!fs.existsSync(__dirname + '/templates/')) {
+        fs.mkdirSync(__dirname + '/templates/');
+    }
+
+    filesTemplate  += toSource(filesList);
+     projectStructureTemplate += toSource(projectStructure);
+
+    if (fs.existsSync(templatesDirPath)) {
+        removeDirectory(templatesDirPath);
+    }
+
+    fs.mkdirSync(templatesDirPath);
+
+    fs.writeFile(templatesDirPath + '/files.js.gz', zlib.gzipSync(filesTemplate), function(error) {
+        if (error) {
+            console.log(error);
+            console.log('File "files.js.gz" ' + ' not created');
+
+            return;
+        }
+
+        console.log(templatesDirPath + '/files.js.gz successfully created');
+    });
+
+    fs.writeFile(templatesDirPath + '/projectStructure.js.gz', zlib.gzipSync(projectStructureTemplate), function(error) {
+        if (error) {
+            console.log(error);
+            console.log('File "projectStructure.js.gz" ' + ' not created');
+
+            return;
+        }
+
+        console.log(templatesDirPath + '/projectStructure.js.gz successfully created');
+    });
+};
+
+function removeDirectory(path) {
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file,index){
+            const curPath = path + "/" + file;
+            if(fs.lstatSync(curPath).isDirectory()) {
+                removeDirectory(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+function createGzip(file) {
+    const gzip = zlib.createGzip();
+    const fs = require('fs');
+    const inp = fs.createReadStream(file);
+    const out = fs.createWriteStream(file + '.gz');
+
+    inp.pipe(gzip).pipe(out);
+};
